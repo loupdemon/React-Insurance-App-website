34a3f405636b6e541de1a016b639d88c95ecbbea
stronk-exe <ael-asri@student.1337.ma>
2022-05-08 20:12:07 +0100

    setup & header

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { imgs } from '../imgs';
 import '../style/Info.scss';
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className='we-are-different'>
+      <div className='container'>
+        <h2 className='title title1--mod'>We are different</h2>
+        <div className='snappy'>
+          <img src={imgs.snappy_process} alt='' />
+          <h3>Snappy Process</h3>
+          <p>
+            Our application process can be completed in mints, not hours.
+            Don't get stuck filling in tedious forms.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
